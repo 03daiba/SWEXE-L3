92dac51e045343fe4b1e29fd4a397453dbde2557
Ubuntu <ubuntu@ip-172-31-83-240.ec2.internal>
2022-09-14 08:15:55 +0000

    ActionView::Template::Error (undefined method `getTime' for Wed, 14 Sep 2022 16:57:28 JST +09:00:Time

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Myapp
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
-    gem 'rails-i18n'
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
